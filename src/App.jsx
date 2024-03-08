2c8254e134b670cab3582838211872bcf4540ae4
domonikk <domonique.eccleston@gmail.com>
2024-03-07 23:22:15 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+      <Header />
+    </>
+  );
 }
 
 export default App;
